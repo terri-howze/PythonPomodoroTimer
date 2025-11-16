a472b50be2cbb3adda6b2b95683b75691204cea8
terri-howze <Bossmods239@gmail.com>
2025-11-16 00:44:44 -0500

    Added cycles selection

--- main.py
+++ main.py
@@ -13,23 +13,46 @@ class Pomodoro:
         master.title  = ("Productivity Jam")
 
         self.minutes = 0
+        self.minutesText = tk.StringVar()
         self.seconds = 0
         self.running = False
-        self.cycles = 2
+        self.cycles = 0
         self.longBreak  = 0
         self.shortBreak = 0
 
-        self.label1 = tk.Label(master, text="1:00", font=(font_name, 25))
+        self.timeLabel = tk.Label(master, text="0:00", font=(font_name, 25))
+        self.label2 = tk.Label(master, textvariable=self.minutesText, font=(font_name, 25))
+
+        studyLabel = tk.Label(master, text="Study Time: ", font=(font_name, 25))
+        shortBreakLabel = tk.Label(master, text="Short Break: ", font=(font_name, 25))
+        longBreakLabel = tk.Label(master, text="Long Break: ", font=(font_name, 25))
+        cyclesLabel = tk.Label(master, text="Cycles: ", font=(font_name, 25))
+
+
+
         #tk.Button(master, text="1", comm)
-        studyTimer1Button = ttk.Button(master, text="1", command=self.setTime1)
-        studyTimer2Button = ttk.Button(master, text="2", command=self.setTime2)
-        studyTimer3Button = ttk.Button(master, text="3", command=self.setTime3)
+        studyTimer1Button = ttk.Button(master, text="1 Minute", command=self.setTime1)
+        studyTimer2Button = ttk.Button(master, text="2 Minutes", command=self.setTime2)
+        studyTimer3Button = ttk.Button(master, text="3 Minutes", command=self.setTime3)
+        studyCyclesButton1 = ttk.Button(master, text="3 Cycles", command=lambda: self.setCycles(3))
+        studyCyclesButton2 = ttk.Button(master, text="4 Cycles", command=lambda: self.setCycles(4))
+        studyCyclesButton3 = ttk.Button(master, text="5 Cycles", command=lambda: self.setCycles(5))
         startButton = tk.Button(master, text="Start", command=self.start)
         resetButton = tk.Button(master, text="Reset", command=self.reset)
-        self.label1.grid(row=0, column=2)
-        studyTimer1Button.grid(row = 1, column= 1)
-        studyTimer2Button.grid(row=1, column=2)
-        studyTimer3Button.grid(row=1, column=3)
+        self.timeLabel.grid(row=0, column=2)
+        self.label2.grid(row=1, column=2)
+
+        studyLabel.grid(row=2, column=1)
+        studyTimer1Button.grid(row = 2, column= 2)
+        studyTimer2Button.grid(row=2, column=3)
+        studyTimer3Button.grid(row=2, column=4)
+
+        cyclesLabel.grid(row=3, column=1)
+        studyCyclesButton1.grid(row=3, column=2)
+        studyCyclesButton2.grid(row=3, column=3)
+        studyCyclesButton3.grid(row=3, column=4)
+
+
         startButton.grid(row = 4, column=1)
         resetButton.grid(row=4, column=3)
 
@@ -41,29 +64,41 @@ class Pomodoro:
 
 
     def countdown(self):
-        studyTotal = self.minutes * 60 + self.seconds
-        for x in self.cycles:
+        studyTotal = self.minutes * 60
+    
+        for x in range(self.cycles):
             while studyTotal >= 0 and self.running:
-                mins, secs = divmod(total, 60)
-                self.label1.config(text=f"{mins:02}:{secs:02}")
+                mins, secs = divmod(studyTotal, 60)
+                self.timeLabel.config(text=f"{mins:02}:{secs:02}")
                 time.sleep(1)
-                total -= 1
-            self.cycles -=1
+                studyTotal -= 1
+            studyTotal = self.minutes * 60
         self.running = False
+        self.minutes = 0
+        self.cycles = 0
 
     def reset(self):
         self.running = False
         self.minutes, self.seconds = 1, 0
-        self.label.config(text="0:00")
+        self.timeLabel.config(text="0:00")
 
     def setTime1(self):
         self.minutes = 1
+        self.minutesText.set("1")
     
     def setTime2(self):
         self.minutes = 2
+        self.minutesText.set("2")
 
     def setTime3(self):
         self.minutes = 3
+        self.minutesText.set("3")
+
+    
+    def setCycles(self,value):
+        self.cycles = value
+        print("Cycles set to:", self.cycles)
+        
 
 
 
